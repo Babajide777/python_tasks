794321e0d2eed4030f94ba1cc69ecf1849d94a85
Babajide Peter Oyafemi <j.oyafemi@gmail.com>
2026-02-24 11:41:09 +0000

    Enhance main.py with Spotify API integration and user token handling
    - Commented out the print statement for Python version
    - Added user_id environment variable retrieval
    - Implemented Spotify API token prompt and user data retrieval
    - Updated print statements to display token and Spotify object if successful

--- main.py
+++ main.py
@@ -8,11 +8,25 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-print(sys.version)
+# print(sys.version)
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 redirect_uri = os.getenv("REDIRECT_URI")
 user_name = os.getenv("USER_NAME")
+user_id = os.getenv("USER_ID")
 
-print(client_id, client_secret, redirect_uri, user_name)
+# print(client_id, client_secret, redirect_uri, user_name, user_id)
+
+scope = "user-library-read user-read-recently-played"
+
+token = spotipy.util.prompt_for_user_token(username=user_name, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    print(f"Token: {token} \n Spotify Object: {sp}")    
+else:
+    print("Failed to get token")
+    
+user_data = sp.current_user()
+    
