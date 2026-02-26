ecec89437a44f9753607245ecd08e9037393dba6
Babajide Peter Oyafemi <j.oyafemi@gmail.com>
2026-02-26 15:42:26 +0000

    Add data validation in main.py for recently played tracks
    - Introduced a new function to check the uniqueness and existence of data in the DataFrame
    - Enhanced user feedback by printing messages based on data validation results
    - Maintained existing functionality while improving data integrity checks

--- main.py
+++ main.py
@@ -8,36 +8,41 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# print(sys.version)
-
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 redirect_uri = os.getenv("REDIRECT_URI")
 user_name = os.getenv("USER_NAME")
 user_id = os.getenv("USER_ID")
 
-# print(client_id, client_secret, redirect_uri, user_name, user_id)
-
 scope = "user-library-read user-read-recently-played"
+
+def check_data(song_df: pd.DataFrame) -> bool:    
+    if song_df.empty:
+        print("Data does not exist")
+        return False
+    else:
+        if pd.Series(song_df['time_played']).is_unique:
+            print("Data already exists")
+            return True
+        else:
+            print("Data is not unique")
+            return False
 
 try:
     token = spotipy.util.prompt_for_user_token(username=user_name, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
     
     if token:
         sp = spotipy.Spotify(auth=token)
-        print(f"Token: {token}")
         
         try:
             user_data = sp.current_user()
             print(f"User data retrieved successfully: {user_data}")
             print(f"User display name: {user_data['display_name']}")
             print(f"User followers count: {user_data['followers']['total']}")
-            print(f"User link: {user_data['external_urls']['spotify']}")
-            
+            print(f"User link: {user_data['external_urls']['spotify']}")            
             
             recently_played = sp.current_user_recently_played(limit=5)
             recently_played_tracks = recently_played['items']
-            print(f"Recently played tracks: {recently_played_tracks}")     
             
             song_names = []
             artist_names = []
@@ -53,6 +58,12 @@ try:
                 'time_played': time_played
             }
             song_df = pd.DataFrame(song_dictionary)
+            
+            if check_data(song_df):                
+                print("Data already exists")
+            else:            
+                print("Data does not exist")
+            
             print(f"Song dataframe: {song_df}")
             song_df.to_csv('recently_played_tracks.csv', index=False)
             
